f065c052c726d38a1b759f1ff5d7efa5d754b5e9
WIN10NHS40360\syota <bora00940@gmail.com>
2017-05-12 15:00:25 +0900

    キャラが今どこのマスにいるか視覚的に分かるように変更

---
+++ CESA_Prototype_01/Assets/Scripts/Charactor/CharaMass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+using UniRx.Triggers;
+
+public class CharaMass : FieldObjectBase {
+
+    SpriteRenderer _SpRend = null;
+    TriangleWave<Color> _triangleWaveColor = null;
+
+    [SerializeField] float _fPeriod_Sec = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //  座標更新処理
+        this.UpdateAsObservable()
+            .Where(_ => this.enabled)
+            .Subscribe(_ =>
+                {
+                    int number = GetDataNumber(transform.parent.position);
+                    transform.position = GetPosForNumber(number);
+                });
+
+        //  拡縮運動
+        _SpRend = GetComponent<SpriteRenderer>();
+        _triangleWaveColor = TriangleWaveFactory.Color(new Color(1,1,1,0), new Color(1,1,1,1), _fPeriod_Sec / 2.0f);
+        this.UpdateAsObservable()
+            .Where(_ => this.enabled)
+            .Subscribe(_ =>
+                {
+                    _triangleWaveColor.Progress();
+                    _SpRend.color = _triangleWaveColor.CurrentValue;
+                });
+    }
+}
