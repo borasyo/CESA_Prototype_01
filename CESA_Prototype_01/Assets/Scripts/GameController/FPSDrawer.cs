82086c22b811c7f55cb8202dc3dd31f147cf3044
WIN10NHS40360\syota <bora00940@gmail.com>
2017-05-21 11:12:22 +0900

    端末デバッグ時に画面にFPSを表示するよう変更

---
+++ CESA_Prototype_01/Assets/Scripts/GameController/FPSDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+
+public class FPSDrawer : MonoBehaviour
+{
+    void Start()
+    {
+        Text text = GetComponent<Text>();
+        float time = 1.0f;
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                time += Time.deltaTime;
+
+                if (time < 1.0f)
+                    return;
+
+                text.text = "FPS : " + Mathf.FloorToInt(1.0f / Time.deltaTime).ToString();
+                time = 0.0f;
+            });
+    }
+
+}
